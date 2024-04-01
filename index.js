541009aa5025bb9d08d6f12f7523336305145726
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-04-01 07:46:28 +0700

    style: revamp hero bg

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function heroBackground() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < drops.length; i++) {
       const text = letters[Math.floor(Math.random() * letters.length)];
-      ctx.fillStyle = "rgba(31, 169, 200, 0.221)";
+      ctx.fillStyle = "rgba(31, 169, 200, 0.421)";
       ctx.fillText(text, i * fontSize, drops[i] * fontSize);
       drops[i]++;
       if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
