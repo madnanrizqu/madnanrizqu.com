54ca8e3f9a8902863bcc0fb5417a0d4840e6cb7d
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-04-06 17:03:24 +0700

    refactor: move mobile nav code to navbar astro

--- public/index.js
+++ public/index.js
@@ -64,41 +64,7 @@ function heroAnimatedHeadline() {
   }
 }
 
-function initDrawerToggleListener() {
-  const toggler = document.getElementById("mobile_drawer_toggle");
-  toggler.addEventListener("click", toggleMobileDrawer);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   heroBackground();
   heroAnimatedHeadline();
-  initDrawerToggleListener();
 });
-
-function toggleMobileDrawer() {
-  const toggler = document.getElementById("mobile_drawer_toggle");
-  const drawer = document.getElementById("mobile_drawer");
-  const [openIcon, closeIcon] = toggler.children;
-
-  const hideClassName = "u_hide_all";
-
-  if (closeIcon.classList.contains(hideClassName)) {
-    // Open drawer
-
-    // Change icons
-    closeIcon.classList.remove(hideClassName);
-    openIcon.classList.add(hideClassName);
-
-    // Show drawer
-    drawer.classList.remove(hideClassName);
-  } else {
-    // Close drawer
-
-    // Change icons
-    closeIcon.classList.add(hideClassName);
-    openIcon.classList.remove(hideClassName);
-
-    // Hide drawer
-    drawer.classList.add(hideClassName);
-  }
-}
