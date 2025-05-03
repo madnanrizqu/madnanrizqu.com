05713501a58b63c8a2ca6985baff721043171cb4
Muhammad Adnan Rizqullah <madnanrizqullah@gmail.com>
2025-05-03 21:50:31 +0300

    move to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [mdx(), tailwind(), react()],
   output: "static",
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
